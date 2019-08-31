33ec098187b9123292d95b206a78918857872bc3
dmeadows <meadows.david@siemens.com>
2019-08-31 15:59:48 -0400

    Added code for the requesthandler. Added code for the readFile callback.

--- server.js
+++ server.js
@@ -1,52 +1,73 @@
-// Making a change so that I can demonstrate a pull
-var http = require('http'), 
-    fs = require('fs'), 
+var http = require('http'),
+    fs = require('fs'),
     url = require('url'),
     port = 8080;
 
 /* Global variables */
 var listingData, server;
 
-var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+var requestHandler = function (request, response) {
+    var parsedUrl = url.parse(request.url);
 
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
+    if (parsedUrl.pathname == '/listings') {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write(listingData);
+        response.end();
+        // send listingData in the JSON format as a response.
+    } else {
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.end();
+    }
+    /*
+      Your request handler should send listingData in the JSON format as a response if a GET request
+      is sent to the '/listings' path. Otherwise, it should send a 404 error.
+
+      HINT: Explore the request object and its properties
+      HINT: Explore the response object and its properties
+      https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
+      http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
+
+      HINT: Explore how callback's work
+      http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
+
+      HINT: Explore the list of MIME Types
+      https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
+     */
 };
 
-fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+fs.readFile('listings.json', 'utf8', function (err, data) {
+    /*
+      This callback function should save the data in the listingData variable,
+      then start the server.
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+      HINT: Check out this resource on fs.readFile
+      //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
 
-    HINT: Read up on JSON parsing Node.js
-   */
+      HINT: Read up on JSON parsing Node.js
+     */
+
+    try {
+        let file_read = fs.createReadStream('listings.json');
+        file_read.on('data', (data) => {
+            listingData = data;
+        });
+    } catch (err) {
+        console.log(err);
+    }
+
 
     //Check for errors
-  
 
-   //Save the sate in the listingData variable already defined
-  
 
-  //Creates the server
-  
-  //Start the server
+    //Save the sate in the listingData variable already defined
 
+
+    //Creates the server
+
+    //Start the server
+    server = http.createServer(requestHandler).listen(port);
 
 });
+
+
