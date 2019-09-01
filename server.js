a59a45a5f959efff3b816f339977535163421125
dmeadows <meadows.david@siemens.com>
2019-08-31 22:36:49 -0400

    added response write "Bad gateway error" if pathname is not "/listings". Cleaned up code and comments.

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-var http = require('http'),
+const http = require('http'),
     fs = require('fs'),
     url = require('url'),
     port = 8080;
 
 /* Global variables */
-var listingData, server;
+let listingData, server;
+
+let requestHandler = function (request, response) {
+    let parsedUrl = url.parse(request.url);
 
 
-var requestHandler = function (request, response) {
-    var parsedUrl = url.parse(request.url);
+    /*
+      The request handler should send listingData in the JSON format as a response if a GET request
+      is sent to the '/listings' path.
+      Otherwise, it should send a 404 error.
+     */
 
     if (parsedUrl.pathname == '/listings') {
         response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -17,34 +23,16 @@ var requestHandler = function (request, response) {
         // send listingData in the JSON format as a response.
     } else {
         response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.write('Bad gateway error');
         response.end();
     }
-    /*
-      Your request handler should send listingData in the JSON format as a response if a GET request
-      is sent to the '/listings' path. Otherwise, it should send a 404 error.
 
-      HINT: Explore the request object and its properties
-      HINT: Explore the response object and its properties
-      https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-      http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-
-      HINT: Explore how callback's work
-      http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-
-      HINT: Explore the list of MIME Types
-      https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-     */
 };
 
 fs.readFile('listings.json', 'utf8', function (err, data) {
     /*
       This callback function should save the data in the listingData variable,
       then start the server.
-
-      HINT: Check out this resource on fs.readFile
-      //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-
-      HINT: Read up on JSON parsing Node.js
      */
 
     try {
@@ -56,18 +44,8 @@ fs.readFile('listings.json', 'utf8', function (err, data) {
         console.log(err);
     }
 
-
-    //Check for errors
-
-
-    //Save the sate in the listingData variable already defined
-
-
-    //Creates the server
-
-    //Start the server
+    //Creates the server and starts it.
     server = http.createServer(requestHandler).listen(port);
-
 });
 
 
